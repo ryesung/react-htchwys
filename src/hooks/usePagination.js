72cc67683803d3c278bd61e9899b57aa7b3c2757
hatchy-reviewer <84484806+hatchy-reviewer@users.noreply.github.com>
2022-11-02 12:51:11 -0300

    Initial commit

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,11 @@
+export const DOTS = "...";
+
+function usePagination() {
+  /*
+    Rewrite the logic here to map out the pagination to be displayed.
+    
+  */
+  return [1, 2, 3, DOTS, 5];
+}
+
+export default usePagination;
